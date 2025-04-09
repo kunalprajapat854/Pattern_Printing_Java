6bcc208d870dffcaa3784d4b37c1f2df9696f683
Kunal <virajprajapat854@gmail.com>
2025-04-09 19:43:24 +0530

    commit

---
+++ src/in/kunal/Hollow_Rectangle.java
@@ -0,0 +1,31 @@
+package in.kunal;
+
+public class Hollow_Rectangle {
+	
+//	output
+//	*****
+//	*   *
+//	*   *
+//	*****
+	
+	public static void main(String[] args) {
+		
+		int n = 4;
+		int m = 5;
+		
+		for(int i=1 ; i<=n;i++) {
+			for(int j=1;j<=m ;j++) {
+				// cell of i j 
+				if(i==1 || j==1|| i==n|| j==m) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
