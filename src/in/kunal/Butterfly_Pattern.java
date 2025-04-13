9f9c0cc8610b7c70835deee258846b87cfbc5756
Kunal <virajprajapat854@gmail.com>
2025-04-13 14:22:27 +0530

    update

--- src/in/kunal/Butterfly_Pattern.java
+++ src/in/kunal/Butterfly_Pattern.java
@@ -4,9 +4,20 @@ public class Butterfly_Pattern {
 	
 	public static void main(String[] args) {
 		
-//	   output
+//	    output
+//		*      *
+//		**    **
+//		***  ***
+//		********
+//		********
+//		***  ***
+//		**    **
+//		*      *
 //	  
 		int n = 4 ; 
+		
+		//upper part
+		
 		for( int i = 1 ;i<=n ;i++) {
 			for(int j = 1 ; j<=i;j++) {
 				System.out.print("*");
