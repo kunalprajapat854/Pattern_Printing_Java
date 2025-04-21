ca57dbb9deeb2ebb04908bdf8a89bdda832c71eb
Kunal <virajprajapat854@gmail.com>
2025-04-21 23:40:10 +0530

    add class

---
+++ src/in/kunal/Asc_Dsc_triangle.java
@@ -0,0 +1,17 @@
+package in.kunal;
+import java.util.Scanner;
+public class Asc_Dsc_triangle {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();  // 5
+		
+		for(int i = 5 ;i>=1;i--) {
+			System.out.print(i);
+		}
+		System.out.println();
+		
+	}
+
+}
