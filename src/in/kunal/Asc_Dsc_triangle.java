d3761449edca8a61ece99a9092b233558ac62aa2
Kunal <virajprajapat854@gmail.com>
2025-04-22 00:01:43 +0530

    update code

--- src/in/kunal/Asc_Dsc_triangle.java
+++ src/in/kunal/Asc_Dsc_triangle.java
@@ -4,13 +4,32 @@ public class Asc_Dsc_triangle {
 	
 	public static void main(String[] args) {
 		
+		
+//		output
+//		1
+//		232
+//		34543
+//		4567654
+//		567898765
+
+		
 		Scanner scanner = new Scanner(System.in);
+		 System.out.print("Enter N: ");
 		int n = scanner.nextInt();  // 5
 		
-		for(int i = 5 ;i>=1;i--) {
-			System.out.print(i);
+		for(int i = 1 ; i<=n ; i++) {
+			int num = i;
+			for(int j = 1 ; j<= i; j++) {
+				System.out.print(num++);
+			}
+			 
+			num -= 2;
+			for(int j = 1 ; j<i;j++) {
+				System.out.print(num--);
+			}
+			System.out.println();
 		}
-		System.out.println();
+		
 		
 	}
 
